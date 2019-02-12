121eacceaaca0ea1da834dfb09e570ee1fba84c0
jayant.fegade <jayant.fegade@protegrity.com>
2019-02-12 13:59:02 +0530

    added lang3 dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,23 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
-    
+
     public Application() {
         System.out.println ("Inside Application");
     }
 
+    public int wordcount(String words) {
+      String[] seperateWords = StringUtils.split(words,' ');
+      return(seperateWords == null) ? 0 : seperateWords.length;
+    }
+
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
+      System.out.println ("Starting Application");
+	    Application app = new Application();
+      int cnt = app.wordcount("this is sparta");
+      System.out.println("number of words: "+ cnt);
     }
 }
